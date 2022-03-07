1995983aebd9220d74801201ea171f217f325684
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-03-07 21:24:18 +0330

    add first code

---
+++ JavaScript/app.js
@@ -0,0 +1,8 @@
+const inputNumber = document.querySelector(".inputNumber"),
+  selectorCategory = document.querySelector("#inputSelectCategory"),
+  selectorDifficulty = document.querySelector("#inputSelectDifficulty"),
+  selectorType = document.querySelector("#inputSelectDifficulty");
+
+const createCategorySelector = async () =>{
+    
+}
